538ea777dab7503d43d6d469ff1641243be8badd
Jean Altidor <jeanaltidor@Jeans-MacBook-Pro.local>
2018-05-30 14:46:22 -0400

    removed unneed controller and dependecies

--- src/main/java/ja/SpringbootMongodbApplication.java
+++ src/main/java/ja/SpringbootMongodbApplication.java
@@ -1,11 +1,9 @@
-package ja.springbootmongodb;
+package ja;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
 @EnableAutoConfiguration
@@ -14,13 +12,4 @@ public class SpringbootMongodbApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootMongodbApplication.class, args);
 	}
-}
-@RestController("/api1")
-class MyController{
-
-	@GetMapping
-	public String getHello(){
-		return "hello";
-	}
-
 }
