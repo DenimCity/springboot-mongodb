b9f4e90005b5646dc7d84ead13e49cc8a5663a0e
Jean Altidor <jeanaltidor@Jeans-MacBook-Pro.local>
2018-05-30 15:19:38 -0400

    get all, get by ID, delete by ID work

--- src/main/java/ja/controller/HotelController.java
+++ src/main/java/ja/controller/HotelController.java
@@ -2,32 +2,69 @@ package ja.controller;
 
 
 import ja.domain.Hotel;
-import ja.repository.HotelRespository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import ja.repository.HotelRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
 
 import java.util.List;
+import java.util.Optional;
+import java.util.logging.Logger;
 
 @RestController
-@RequestMapping("/hotels")
 public class HotelController {
-    private HotelRespository hotelRespository;
 
-    public HotelController(HotelRespository hotelRespository){
-        this.hotelRespository = hotelRespository;
+
+    private HotelRepository hotelRepository;
+
+    @Autowired
+    public HotelController(HotelRepository hotelRepository){
+        this.hotelRepository = hotelRepository;
     }
+
+
+    Logger LOGGER = Logger.getLogger(HotelController.class.getName());
 
     @GetMapping("/")
     public List<Hotel> getall(){
-    List<Hotel> hotels = this.hotelRespository.findAll();
+        LOGGER.info("*************************Grabbing all Hotels*************************");
+    List<Hotel> hotels = this.hotelRepository.findAll();
 
     return hotels;
     }
 
 
+    @GetMapping("/{id}")
+    public Optional<Hotel> getById(@PathVariable("id") String id){
+        Optional<Hotel> hotel = this.hotelRepository.findById(id);
+
+        return hotel;
+    }
 
 
+
+    @PostMapping("/new")
+    public void insert(@RequestBody Hotel hotel){
+        LOGGER.info("*************************Creating New Hotels*************************");
+        //we use insert here to generate an initial id to create
+        this.hotelRepository.insert(hotel);
+    }
+
+//    @PostMapping("/new")
+//    public void update(@RequestBody Hotel hotel){
+//        LOGGER.info("*************************Updating Hotel*************************");
+//        //but if were updating then there is already a an ID so we're just saving to an already
+//        //generated ID
+//        this.hotelRepository.save(hotel);
+//    }
+
+    @DeleteMapping("/{id}")
+
+    public void delete(@PathVariable("id") String id){
+        LOGGER.info("*************************Deleting Hotel*************************");
+        this.hotelRepository.deleteById(id);
+
+    }
 
 
 
