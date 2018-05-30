d3a296565dedab7cf4cb06c4fd59e873253d9b54
Jean Altidor <jeanaltidor@Jeans-MacBook-Pro.local>
2018-05-30 15:19:11 -0400

    seeds files now work..

--- src/main/java/ja/dbseeds/DbSeeder.java
+++ src/main/java/ja/dbseeds/DbSeeder.java
@@ -1,64 +1,67 @@
 package ja.dbseeds;
 
-
 import java.util.Arrays;
 import java.util.List;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import ja.domain.Address;
 import ja.domain.Hotel;
 import ja.domain.Review;
-import ja.repository.HotelRespository;
+import ja.repository.HotelRepository;
 
 
 @Component
 public class DbSeeder implements CommandLineRunner {
+    private HotelRepository hotelRepository;
 
-    private HotelRespository hotelRespository;
-
-    public DbSeeder(HotelRespository hotelRespository) {
-        this.hotelRespository = hotelRespository;
+    public DbSeeder(HotelRepository hotelRepository) {
+        this.hotelRepository = hotelRepository;
     }
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... strings) throws Exception {
         Hotel marriot = new Hotel(
+
                 "Marriot",
                 130,
-                new Address("Paris", "France" ),
+                new Address("Paris", "France"),
                 Arrays.asList(
                         new Review("John", 8, false),
                         new Review("Mary", 7, true)
                 )
         );
 
-        Hotel KapelTowers = new Hotel(
-                "Kapel Towers",
+        Hotel ibis = new Hotel(
+                "Ibis",
                 90,
-                new Address("Truth", "Florida" ),
+                new Address("Bucharest", "Romania"),
                 Arrays.asList(
-                        new Review("Jean", 9, true),
-                        new Review("Emma", 7, false)
+                        new Review("Teddy", 9, true),
+                        new Review("Flower", 2, false)
                 )
         );
 
-        Hotel dropal = new Hotel(
-                "Dropal Towers",
+        Hotel sofitel = new Hotel(
+                "Sofitel",
                 200,
-                new Address("Rome", "Italy" ),
+                new Address("Rome", "Italy"),
                 Arrays.asList(
-                        new Review("Michelle", 9, true),
-                        new Review("Ozzie", 7, false)
+                        new Review("Jean", 10, true),
+                        new Review("Wella", 8, true)
+
                 )
         );
 
-        //Drop the existing collection that currently in the database to start with a clean database
-        this.hotelRespository.deleteAll();;
+        // drop all hotels
+        this.hotelRepository.deleteAll();
 
-        //add our hotel seeds
-        List<Hotel> hotels = Arrays.asList(marriot, KapelTowers, dropal);
-        this.hotelRespository.save(hotels);
-
+        //add our hotels to the database
+        List<Hotel> hotels = Arrays.asList(marriot, ibis, sofitel);
+        System.out.println("******************Attempting to save seeded data******************");
+        this.hotelRepository.saveAll(hotels);
+        System.out.println("Hotels being saved to:" + hotels);
+        System.out.println("******************Saved DB****************");
     }
 }
