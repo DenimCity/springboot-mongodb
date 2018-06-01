0cb967566faae6024a311baca3a3759dee6acd57
Jean Altidor <jeanaltidor@Jeans-MacBook-Pro.local>
2018-06-01 08:00:00 -0400

    added a util folder

--- client/src/services/axiosServices.js
+++ client/src/services/axiosServices.js
@@ -1,29 +1,33 @@
-import axios from 'axios'
+import axios from "axios";
 
-const baseUrl = 'http://localhost:8080';
+const baseUrl = "http://localhost:8080";
 
-export let allHotels  = () => {
-   return axios.get(baseUrl)
+export let allHotels = () => {
+  return axios.get(baseUrl);
+};
 
-}
+export let createUser = requestBody => {
+  return axios.get(baseUrl, requestBody);
+};
 
+export let postUser = async userId => {
+  await axios.post(baseUrl + "/" + userId);
+};
 
-export let xyz = (newHotelinfo) => {
-    return axios.put(baseUrl + '/', newHotelinfo )
-}
+export let xyz = newHotelinfo => {
+  return axios.put(baseUrl + "/", newHotelinfo);
+};
 
+export let byID = hotelId => {
+  // multiple ways to write it
+  return axios.get(baseUrl + "/" + hotelId);
+  // return axios.get`${baseUrl}/${hotelId}`
+};
 
-export let byID =(hotelId)=> {
-    // multiple ways to write it
-    return axios.get(baseUrl + '/' + hotelId)
-    // return axios.get`${baseUrl}/${hotelId}`
-}
+export let createHotel = hotelinfo => {
+  return axios.post(baseUrl + "/new", hotelinfo);
+};
 
-export let createHotel = (hotelinfo) =>{
-    return axios.post(baseUrl + '/new', hotelinfo)
-} 
-
-
-export let deleteHote = (hotelId) => {
-    return axios.delete(baseUrl + '/' + hotelId )
-}
+export let deleteHote = hotelId => {
+  return axios.delete(baseUrl + "/" + hotelId);
+};
